0c39cb4f99988f2985201ffbd34397ae06cbdc9b
KUO <drjkuo@gmail.com>
2017-07-01 23:23:21 -0700

    1st commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
+// Create a new component
+// Add some HTML
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/search_bar';
 
-import App from './components/app';
-import reducers from './reducers';
+import YTSearch from 'youtube-api-search';
+const API_KEY = "AIzaSyDO0em5E-2Jg2qr22PyHmL0mddbJZyKraI";
+YTSearch( {key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+})
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector('.container'));
